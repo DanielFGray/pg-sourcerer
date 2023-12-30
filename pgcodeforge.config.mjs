f113d1b2febe40d94fe5048b3df4ed1b2c6b2598
DanielFGray <danielfgray@gmail.com>
2023-12-30 14:32:24 -0600

    fix: rewrite everything

--- pgcodeforge.config.mjs
+++ pgcodeforge.config.mjs
@@ -1,15 +1,24 @@
-import { makeTypesPlugin, createQueriesPlugin } from ".";
+import { makeTypesPlugin, createQueriesPlugin, makeZodSchemasPlugin } from "./index.mjs";
+import { camelize } from "inflection";
 
-const schemas = ["app_public", "app_private"]
+const schemas = ["app_public"];
 
-/** @type {import('./index').Config} */
 export default {
-  adapter: "pg",
+  adapter: "postgres",
   connectionString: process.env.AUTH_DATABASE_URL || process.env.DATABASE_URL,
   role: process.env.DATABASE_VISITOR,
   outputDir: "./generated",
+  inflections: {
+    columnNames: (t) => camelize(t, true),
+  },
+  typeMap: {
+    "app_public.username": "string",
+    "app_public.url": "string",
+    "public.citext": "string",
+  },
   plugins: [
-    makeTypesPlugin({ schemas }),
-    createQueriesPlugin({ schemas }),
+    makeTypesPlugin({ schemas, path: (t) => `./types.ts` }),
+    makeZodSchemasPlugin({ schemas, path: (t) => `./schemas.ts` }),
+    createQueriesPlugin({ schemas, path: (t) => `./queries.ts` }),
   ],
 };
